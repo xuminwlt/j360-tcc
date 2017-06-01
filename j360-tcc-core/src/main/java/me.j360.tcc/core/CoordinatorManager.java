72dc7af93c1ca51fa80597173beef5135db96e24
min_xu <xumin_wlt@163.com>
2017-06-01 11:38:48 +0800

    add core tcc class

--- j360-tcc-core/src/main/java/me.j360.tcc/core/CoordinatorManager.java
+++ j360-tcc-core/src/main/java/me.j360.tcc/core/CoordinatorManager.java
@@ -1,5 +1,8 @@
 package me.j360.tcc.core;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Package: me.j360.tcc.core
  * User: min_xu
@@ -7,4 +10,23 @@ package me.j360.tcc.core;
  * 说明：协作管理类,面向底层实现
  */
 public class CoordinatorManager {
+
+
+    //服务端的注册列表,具体的执行由rpc完成
+    private Map<String, Object> handlerMap = new HashMap<String, Object>();
+
+
+
+    public <T> T getBean(Class<T> var1) {
+        return (T) this.handlerMap.get(var1.getCanonicalName());
+    }
+
+
+    //执行对应的confirm、rollback方法
+    public Object invoke(TransactionContext transactionContext) {
+        return null;
+    }
+
+
+
 }
---
+++ j360-tcc-spring/src/main/java/me/j360/tcc/spring/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package: me.j360.tcc.spring
+ * User: min_xu
+ * Date: 2017/6/1 上午11:08
+ * 说明：
+ */
+package me.j360.tcc.spring;
